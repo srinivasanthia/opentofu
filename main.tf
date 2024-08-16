9ae886949b0d1cd1a6ee6b5c2611661120a2bb9a
srinivasanthia <31888801+srinivasanthia@users.noreply.github.com>
2024-08-16 13:09:36 +0545

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "myvpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "myvpc"
+  }
+}
+resource "aws_subnet" "mysubnet" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "mysubnet"
+  }
+}
+
+resource "aws_instance" "ubuntu2204" {
+  ami           = "ami-080e1f13689e07408"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.mysubnet.id
+  tags = {
+    Name = "ubuntu2204"
+  }
+}
